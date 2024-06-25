e83bf2d5b13cb7b31d96781f447e77df0303a87c
Jose Miguel Millan <jevo160296@gmail.com>
2024-06-24 19:56:07 -0500

    menu solution

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-def main():
+def hello_world():
+    print("Hello python")
+
+def hello_name():
+    name = input("Enter your name: ")
+    print(f"Hello {name}.")
+
+def numerical_input():
     number1 = int(input("Enter a number: "))
     number2 = int(input("Enter another number: "))
     print(f"The first number is {number1} the second number is {number2}")
+
+def main():
+    print("""
+    1. Hello world.
+    2. Hello name.
+    3. Numerical input.
+    """)
+    option = int(input("Select an option: "))
+    if option == 1:
+        hello_world()
+    elif option == 2:
+        hello_name()
+    elif option == 3:
+        numerical_input()
 
 if __name__ == "__main__":
     main()
