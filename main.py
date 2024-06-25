03659abf63ff619142a522bf292a614f343ab087
Jose Miguel Millan <jevo160296@gmail.com>
2024-06-24 19:56:07 -0500

    Numerical input solution

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
-    name = input("Enter your name: ")
-    print(f"Hello {name}.")
+    number1 = int(input("Enter a number: "))
+    number2 = int(input("Enter another number: "))
+    print(f"The first number is {number1} the second number is {number2}")
 
 if __name__ == "__main__":
     main()
